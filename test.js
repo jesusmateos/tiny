41a0e4f9e145ae99e98faa2bb665b7e3972031e9
jesusmateos <jesusmmateos@outlook.com>
2014-12-15 23:15:58 +0100

    Make in so

---
+++ test.js
@@ -0,0 +1,16 @@
+.addEventListener(, function(e){  })
+document.addEventListener('click', function(e){
+  console.log();
+})
+.addEventListener('', function(e){
+
+})
+
+document.addEventListener('click', function(e){
+
+})
+
+(function() {
+  "use strict";
+  
+}());
